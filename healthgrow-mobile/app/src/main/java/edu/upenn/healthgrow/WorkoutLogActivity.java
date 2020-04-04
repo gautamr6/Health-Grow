024f2b187aa60373d42a3df16c3f70288d9af216
bbae07 <16512832+bbae07@users.noreply.github.com>
2020-04-05 05:37:47 +0900

    setup node, mongo + impl. image selection

--- healthgrow-mobile/app/src/main/java/edu/upenn/healthgrow/WorkoutLogActivity.java
+++ healthgrow-mobile/app/src/main/java/edu/upenn/healthgrow/WorkoutLogActivity.java
@@ -14,10 +14,46 @@ import java.io.InputStream;
 
 public class WorkoutLogActivity extends AppCompatActivity {
 
+    private ImageView workoutImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_log);
+
+        workoutImg = findViewById(R.id.workoutimg);
+
+        workoutImg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    workoutImg.setImageBitmap(img);
+                } catch (Exception e) { }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled Photo Selection", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void onClickSave(View view) {
+        
     }
 
 }
