61b44758c2129bc480af002cdca9f45076ad3cde
Kevin Sun <kevinsun0@gmail.com>
2020-04-05 13:51:54 -0400

    add react backend admin and exercise routes

---
+++ healthgrow-web/backend/models/workout.model.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    workout: { type: String, required: true },
+    reps: { type: Number },
+    weight: { type: Number },
+    img: { data: Buffer, contentType: String },
+  }, {
+    timestamps: true,
+  });
+
+const Workout = mongoose.model('Workout', workoutSchema);
+module.exports = Workout
