54ec5fd87eac5b30960c6ac6b8d413c0f84a88c9
Gautam Ramesh <gautamshivramesh@gmail.com>
2020-04-25 23:00:36 -0400

    finished daily alert

--- healthgrow-mobile/app/src/main/java/edu/upenn/healthgrow/AlarmReceiver.java
+++ healthgrow-mobile/app/src/main/java/edu/upenn/healthgrow/AlarmReceiver.java
@@ -8,9 +8,24 @@ import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
+    private DataSource dataSource;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Notification", "Starting action");
-        Toast.makeText(context, "Have you logged your mood today?", Toast.LENGTH_LONG).show();
+
+        dataSource = new DataSource();
+        //String email = intent.getStringExtra("email");
+        String email = MainActivity.email;
+        if (email == null) {
+            Log.d("Notification", "null email");
+        }
+
+        if (dataSource.isMood(email)) {
+            Log.d("Notification", "yes mood");
+        } else {
+            Log.d("Notification", "no mood");
+            Toast.makeText(context, "You haven't logged your mood today!", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
