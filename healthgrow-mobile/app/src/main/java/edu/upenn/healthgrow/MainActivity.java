c8efdf50767630087399b44b2089110ea00ec6b6
Gautam Ramesh <gautamshivramesh@gmail.com>
2020-04-25 20:48:05 -0400

    added alert to log mood, doesn't check whether user has already logged mood yet

--- healthgrow-mobile/app/src/main/java/edu/upenn/healthgrow/MainActivity.java
+++ healthgrow-mobile/app/src/main/java/edu/upenn/healthgrow/MainActivity.java
@@ -1,19 +1,28 @@
 package edu.upenn.healthgrow;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class MainActivity extends AppCompatActivity {
 
     private boolean isSignedIn;
     private String email;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +31,37 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         isSignedIn = intent.getBooleanExtra("signedIn", false);
         email = intent.getStringExtra("email");
+
+        Log.d("Notification", "onCreate");
+
         if (isSignedIn) {
+            Log.d("Notification", "isSignedIn");
+
             String newText = "Signed In as " + email;
             ((TextView) findViewById(R.id.textView)).setText(newText);
-        }
 
+
+            start();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void start() {
+        Log.d("Notification", "start method");
+
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, alarmIntent, 0);
+
+        int interval = 1000 * 30;
+
+        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, 20);
+//        calendar.set(Calendar.MINUTE, 2);
+//        calendar.set(Calendar.SECOND, 0);
+
+//        manager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), interval, pendingIntent);
     }
 
 //    protected void onSaveInstanceState(Bundle state) {
